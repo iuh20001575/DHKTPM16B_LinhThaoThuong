34a8bcddd716106828fdd7e75e68be98c0278baf
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-10-10 18:35:26 +0700

    Unit test lab 06

---
+++ UTLab06.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestBlack_box
+{
+    [TestClass]
+    public class UTLab06
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+
+            String actualResult = methodLibrary.ThayThe("", "b", "d");
+            String expectedResult = "";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+
+            String actualResult = methodLibrary.ThayThe("abc", "", "d");
+            String expectedResult = "abc";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+
+            String actualResult = methodLibrary.ThayThe("abc", "b", "");
+            String expectedResult = "ac";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+
+            String actualResult = methodLibrary.ThayThe("abc", "b", "d");
+            String expectedResult = "";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
